43e81c0a8af0bc806213087325b5d0df296d6726
pazukdev <pazukdev@gmail.com>
2019-11-30 14:23:17 +0300

    storage and router moved to plugins directory

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from "@/router";
-import store from "./store";
+import router from '@/plugins/router'
+import store from '@/plugins/store';
 import {i18n} from "@/plugins/i18n";
 
 Vue.config.productionTip = false;
