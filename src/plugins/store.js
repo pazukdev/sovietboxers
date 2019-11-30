6d34bb57c63794b1b6316d21ce1ce75921cc0856
pazukdev <pazukdev@gmail.com>
2019-11-30 14:25:42 +0300

    dictionary.js moved to modules directory

--- src/plugins/store.js
+++ src/plugins/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import dictionary from '@/plugins/dictionary'
+import dictionary from '@/modules/dictionary'
 import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
